96b618b05f39b5ea1be4603da7e6844ef55f1d4e
MunrraMT <dev.armsantos@gmail.com>
2023-06-16 02:13:49 -0400

    refactor User build!/1 test use Map.get

--- test/elixir_api_restaurant/user_test.exs
+++ test/elixir_api_restaurant/user_test.exs
@@ -6,27 +6,29 @@ defmodule ElixirApiRestaurant.UserTest do
   doctest(User)
 
   describe "build!/1" do
-    test "should returns User struct with name, address and phone" do
+    test "should returns User struct" do
       result_build =
         User.build!(%{
           name: "Maria Belizário",
           email: "email@email.com",
-          document: 00_000_000_000,
+          document: 81_403_849_099,
           password: "123456",
           phone: 65_999_999_999,
-          street: "street",
-          number: 15,
-          city: "Várzea Grande",
-          state: "Mato Grosso",
+          address_street: "street",
+          address_number: 15,
+          address_city: "Várzea Grande",
+          address_state: "Mato Grosso",
           zip_code: 78_000_000
         })
 
       %struct_name{} = result_build
 
       assert struct_name == User
-      assert result_build.name == "Maria Belizário"
-      assert result_build.email == "email@email.com"
-      assert result_build.phone == 65_999_999_999
+      assert Map.get(result_build, :name) == "Maria Belizário"
+      assert Map.get(result_build, :email) == "email@email.com"
+      assert Map.get(result_build, :password) == "123456"
+      assert Map.get(result_build, :document) == 81_403_849_099
+      assert Map.get(result_build, :phone) == 65_999_999_999
     end
   end
 end
