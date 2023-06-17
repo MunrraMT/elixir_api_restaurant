d31d86065642740c8fe571d515819662ccb89177
MunrraMT <dev.armsantos@gmail.com>
2023-06-17 00:38:53 -0400

    create Repository save_client!/1 doctest

--- lib/elixir_api_restaurant/repository.ex
+++ lib/elixir_api_restaurant/repository.ex
@@ -1,20 +1,36 @@
 defmodule ElixirApiRestaurant.Repository do
+  @moduledoc """
+  Módulo de repositório
+  """
+
   @db_folder "./persist"
 
   alias ElixirApiRestaurant.Client
 
+  @doc ~S"""
+  Salva o cliente em um arquivo em formato de erlang binário
+
+  ## Parameters
+
+  - Struct de cliente
+
+  ## Examples
+
+  ```elixir
+  iex> Repository.save_client!(%ElixirApiRestaurant.Client{name: "Maria Belizário", email: "email@email.com", document: 81_403_849_099, password: "123456", phone: 65_999_999_999, address_street: "street", address_number: 15, address_city: "Várzea Grande", address_state: "Mato Grosso", zip_code: 78_000_000})
+  ```
+
+  """
   def save_client!(%Client{} = client) do
-    @db_folder
-    |> File.mkdir_p!()
+    @db_folder |> File.mkdir_p!()
 
-    filename =
-      client
-      |> Map.get(:name)
-      |> Slug.slugify()
-
-    "#{@db_folder}/#{filename}"
+    "#{@db_folder}/#{get_filename(client)}"
     |> File.write!(:erlang.term_to_binary(client))
+  end
 
-    :ok
+  defp get_filename(client) do
+    client
+    |> Map.get(:name)
+    |> Slug.slugify()
   end
 end
